193fb4ab693ce76c7c3e9c532688a5a7568542d4
OmarKHDR <omarsliman37@gmail.com>
2024-12-15 13:27:13 +0200

    updating server.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,16 +3,16 @@ import dbClient from '../utils/db';
 
 export default class AppController {
   static getStatus(req, res) {
-    res.status(200).send(`${JSON.stringify({
+    res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-    })}\n`);
+    });
   }
 
   static getStats(req, res) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([usersCount, filesCount]) => {
-        res.status(200).send(`${JSON.stringify({ users: usersCount, files: filesCount })}\n`);
+        res.status(200).json({ users: usersCount, files: filesCount });
       });
   }
 }
