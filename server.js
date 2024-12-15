c0c28fdf936442be2ac97eccd66b8f18581c5aa7
OmarKHDR <omarsliman37@gmail.com>
2024-12-15 10:23:40 +0200

    '

---
+++ server.js
@@ -0,0 +1,9 @@
+import Express from 'express';
+import { env } from 'process';
+import routes from './routes/index'
+
+const app = Express();
+app.use('/', routes);
+app.listen(env.port || 5000, () => {
+  console.log("connected");
+})
