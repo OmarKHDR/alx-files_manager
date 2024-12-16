00d779f7c69711304e51617f2ebbf9200c405397
OmarKHDR <omarsliman37@gmail.com>
2024-12-16 09:36:03 +0200

    fsd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController.js';
+// another way is creating a factory : a function which takes
+// Express app instance as an arg then set its routes
 
 const routes = Router();
 
 routes.get('/status', AppController.getStatus);
 routes.get('/stats', AppController.getStats);
+routes.get('/users', UsersController.postNew);
 
 export default routes;
