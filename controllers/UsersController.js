034e73d1909416ab60ddacf128a0465bb26d903e
OmarKHDR <omarsliman37@gmail.com>
2024-12-17 12:44:52 +0200

    updating controllers/UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
-import dbClient from '../utils/db';
 import crypto from 'crypto';
+import { ObjectId } from 'mongodb';
+import dbClient from '../utils/db';
 
 export default class UsersController {
   static async postNew(req, res) {
@@ -13,13 +14,29 @@ export default class UsersController {
       res.status(400).json({ error: 'Missing password' });
       return;
     }
-    const collection = dbClient.db(dbClient.database).collection(dbClient.collection);
+    const collection = dbClient.client.db(dbClient.database).collection('users');
     const user = await collection.findOne({ email });
     if (user) {
       res.status(400).json({ error: 'already exists' });
       return;
     }
-    collection.insertOne({ email, password: UsersController.sha1(password) });
+    try {
+      const userDb = await collection.insertOne({
+        email,
+        password: UsersController.sha1(password),
+      });
+      const insertId = userDb.insertedId;
+      const result = await collection.findOne({ _id: ObjectId(insertId) });
+      if (userDb) {
+        res.status(200).json({
+          _id: result._id,
+          email: result.email,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    res.end();
   }
 
   static sha1(data) {
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import Express from 'express';
 import { env } from 'process';
+import bodyParser from 'body-parser';
 import routes from './routes/index';
 
 const app = Express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
 app.use('/', routes);
-app.listen(env.port || 5000, () => {
+app.listen(env.port || 5000, '0.0.0.0', () => {
   console.log('connected');
 });
